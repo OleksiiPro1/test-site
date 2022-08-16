27f52e56e6282b707498fd53d3a3932534d73e6f
Alexey Piltenko <ukr.top@ukr.net>
2022-08-16 12:11:57 +0200

    level 1

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { getProduct } = require('./database/db')
+const server = express();
+
+server.set('view engine', 'ejs');
+server.set('views', './views');
+
+server.use(express.static('./public'));
+
+server.get('/', '/product/:id', (req, res)=>{
+  const { id } = req.params;
+
+const product = getProduct( Number(id) );
+console.log('product: ', product);
+
+res.render('main', { product });
+
+  });
+
+
+
+server.listen(3000);
---
+++ database/db.js
@@ -0,0 +1,14 @@
+const allProducts = [
+  {id: 1, title: 'product1', description: 'product1 is very good'},
+  {id: 2, title: 'product2', description: 'product2 is very good'},
+  {id: 3, title: 'product3', description: 'product3 is very good'},
+  {id: 4, title: 'product4', description: 'product4 is very good'},
+]
+
+const getProduct = (id) => {
+  const result = allProducts.find(item => item.id === id);
+  return result;
+}
+module.exports = {
+  getProduct
+}
