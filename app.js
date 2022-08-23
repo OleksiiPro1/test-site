78c450d9dbf176831f749cf055e3909a6ee6ef34
Alexey Piltenko <ukr.top@ukr.net>
2022-08-23 14:06:19 +0200

    commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { getProduct } = require('./database/products')
 const server = express();
-
+const JSON = require('JSON');
 server.set('view engine', 'ejs');
 server.set('views', './views');
 
@@ -15,9 +15,21 @@ server.get('/', (req, res) =>  {
     {id: 4, title: 'Orange', description: 'Orange is very good', url: 'https://gcdn.utkonos.ru/resample/256x256q90/images/recipe/20180803143146-detoks.jpg'},
   ]
 
-res.render('index', {title: 'Main page', products });
+const products_json = JSON.stringify(products);
+
+res.render('index', {title: 'Main page'});
+// res.render('index', {title: 'Main page', products });
 
   });
+  server.get('/data.json', function(req, res, next) {
+    const products = [
+    {id: 1, title: 'Banana', description: 'Banana is very good', url: 'https://www.abcfact.ru/upload/001/u107/304/10fe8665.jpg'},
+    {id: 2, title: 'Apple', description: 'Apple is very good', url: 'https://c-fa.cdn.smule.com/smule-gg-s-sf-bck4/arr/5c/08/c069505a-b7df-45a5-86b2-31188f21b992.jpg'},
+    {id: 3, title: 'Pineapple', description: 'Pineapple is very good', url: 'https://catalogapp.b-cdn.net/production/images/46/71/32/92/preview/a128620b-dcf5-4bbf-af31-4a08701eedf2.unknown'},
+    {id: 4, title: 'Orange', description: 'Orange is very good', url: 'https://gcdn.utkonos.ru/resample/256x256q90/images/recipe/20180803143146-detoks.jpg'},
+  ]
+  const products_json = JSON.stringify(products);
+  var db = req.db; db.myCollection.find(function (err, docs) { if(err) {} else { res.json({products_json}); } }); });
 
 server.get('/product/:id', (req, res)=>  {
   const { id } = req.params;
