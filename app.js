24bf53fff57a84dea009ae317e70026cdb1239c0
Alexey Piltenko <ukr.top@ukr.net>
2022-08-24 17:21:57 +0200

    middleware 1/0

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const { getProduct } = require('./database/products')
 const server = express();
-const JSON = require('JSON');
+
 server.set('view engine', 'ejs');
 server.set('views', './views');
+
+server.use((req, res, next) => {
+  console.log('URL: ', req.url);
+  res.send('Text');
+})
 
 server.use(express.static('./public'));
 
@@ -15,30 +20,19 @@ server.get('/', (req, res) =>  {
     {id: 4, title: 'Orange', description: 'Orange is very good', url: 'https://gcdn.utkonos.ru/resample/256x256q90/images/recipe/20180803143146-detoks.jpg'},
   ]
 
-res.render('index', {title: 'Main page'});
-// res.render('index', {title: 'Main page', products });
+res.render('index', {title: 'Main page', products });
 
   });
-  server.get('/data', function(req, res) {
-    const products = [
-    {id: 1, title: 'Banana', description: 'Banana is very good', url: 'https://www.abcfact.ru/upload/001/u107/304/10fe8665.jpg'},
-    {id: 2, title: 'Apple', description: 'Apple is very good', url: 'https://c-fa.cdn.smule.com/smule-gg-s-sf-bck4/arr/5c/08/c069505a-b7df-45a5-86b2-31188f21b992.jpg'},
-    {id: 3, title: 'Pineapple', description: 'Pineapple is very good', url: 'https://catalogapp.b-cdn.net/production/images/46/71/32/92/preview/a128620b-dcf5-4bbf-af31-4a08701eedf2.unknown'},
-    {id: 4, title: 'Orange', description: 'Orange is very good', url: 'https://gcdn.utkonos.ru/resample/256x256q90/images/recipe/20180803143146-detoks.jpg'},
-  ]
-  const products_json = JSON.stringify(products);
 
-res.json({products_json});
-  });
-server.get('/product/:id', (req, res)=>  {
-  const { id } = req.params;
+// server.get('/product/:id', (req, res)=>  {
+//   const { id } = req.params;
 
-const product = getProduct( Number(id) );
-console.log('product: ', product);
+// const product = getProduct( Number(id) );
+// // console.log('product: ', product);
 
-res.render('main', { product });
+// res.render('main', { product });
 
-  });
+//   });
 
 
 
